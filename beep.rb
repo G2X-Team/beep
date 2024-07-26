7263b81d56005e433c85bc0fe197b2f2c36576c8
Dani Lobaton <dlobaton2@gmail.com>
2024-07-26 13:58:18 -0700

    make executable and tap

--- beep.rb
+++ beep.rb
@@ -1,9 +1,9 @@
 class Beep < Formula
     desc "Simple CLI tool to beep around with in G2X"
     homepage "https://github.com/G2X-Team/beep"
-    url "https://github.com/yourusername/beep/archive/v1.0.0.tar.gz"
-    sha256 "YOUR_TARBALL_SHA256_HERE"
-    version "1.0.0"
+    url "https://github.com/G2X-Team/beep/archive/v0.2.0.tar.gz"
+    sha256 "45e5c4fa90bac24d3b6cf2e0fe82e3579d4cf3101d189448363cfbf5be56d3e4"
+    version "0.2.0"
   
     def install
       bin.install "beep"
